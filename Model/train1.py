1184f67d134f61dd436bbe73d2d613e37c419cd1
Timmy Hussain <timmyhussain@yahoo.com>
2021-11-27 20:49:31 -0800

    done training

--- Model/train1.py
+++ Model/train1.py
@@ -7,13 +7,13 @@ Created on Fri Nov 26 23:19:07 2021
 import json
 from helper import Net
 
-map_size = 10
+map_size = 50
 data_path = "Final_{map_size}{map_size}/".format(map_size=map_size)
 map_path = "../Maps/map1-new/"
 
 paths = [data_path, map_path]
 activations = ["relu", "relu", "relu"]
-net = Net(paths, "NN1", map_size, activations)
+net = Net(paths, "NN3", map_size, activations)
 history = net.train(10000, 64)
 with open(net.path+net.name+'_history.json', 'w') as fp:
     json.dump(history.history, fp)
